d73f995448f4fa0761698346dbfa2f80c0bc5720
ealpago <alpagoemre@icloud.com>
2022-08-27 15:50:10 +0300

    add - CharactersModel structs addded

---
+++ Rick&Morty-API/Models/CharactersModel/CharactersModel.swift
@@ -0,0 +1,14 @@
+//
+//  CharactersModel.swift
+//  Rick&Morty-API
+//
+//  Created by Emre Alpago on 27.08.2022.
+//
+
+import Foundation
+
+
+struct CharactersModel {
+    let info: Info?
+    let results:[Results?] = []
+}
---
+++ Rick&Morty-API/Models/CharactersModel/Results.swift
@@ -0,0 +1,47 @@
+//
+//  Results.swift
+//  Rick&Morty-API
+//
+//  Created by Emre Alpago on 27.08.2022.
+//
+
+import Foundation
+
+struct Results{
+    let id: Int?
+    let name: String?
+    let status: Status?
+    let species: Species?
+    let type: String?
+    let gender: Gender?
+    let origin: Location?
+    let location: Location?
+    let image: String?
+    let episode: [String?] = []
+    let url: String?
+    let created: String?
+}
+
+enum Gender {
+    case female
+    case male
+    case unknown
+}
+
+struct Location {
+    let name: String
+    let url: String
+}
+
+enum Species {
+    case alien
+    case human
+}
+
+enum Status {
+    case alive
+    case dead
+    case unknown
+}
+
+
