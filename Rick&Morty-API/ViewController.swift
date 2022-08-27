d381d93b317cbd0bcea3f5c7eb62480f2934c23f
ealpago <alpagoemre@icloud.com>
2022-08-27 14:22:51 +0300

    Initial Commit

---
+++ Rick&Morty-API/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Rick&Morty-API
+//
+//  Created by Emre Alpago on 27.08.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Rick&Morty-APIUITests/Rick_Morty_APIUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  Rick_Morty_APIUITestsLaunchTests.swift
+//  Rick&Morty-APIUITests
+//
+//  Created by Emre Alpago on 27.08.2022.
+//
+
+import XCTest
+
+class Rick_Morty_APIUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
