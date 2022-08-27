a173fef43cf09895940970b73bc8df2d3880230b
ealpago <alpagoemre@icloud.com>
2022-08-27 18:19:06 +0300

    add - CharacterPage and CharacterCollectionViewCell added

---
+++ Rick&Morty-API/CollectionCells/CharacterCollectionViewCell/CharacterCollectionViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  CharacterCollectionViewCell.swift
+//  Rick&Morty-API
+//
+//  Created by Emre Alpago on 27.08.2022.
+//
+
+import UIKit
+
+class CharacterCollectionViewCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
---
+++ Rick&Morty-API/Scenes/CharacterListPage/CharacterListViewController.swift
@@ -0,0 +1,17 @@
+//
+//  CharacterListViewController.swift
+//  Rick&Morty-API
+//
+//  Created by Emre Alpago on 27.08.2022.
+//
+
+import UIKit
+
+class CharacterListViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+}
